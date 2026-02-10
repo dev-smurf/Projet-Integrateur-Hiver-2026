e551352887cbcf197078460849567b4bd36e1d97
Fortin-Philippe <159812164+Fortin-Philippe@users.noreply.github.com>
2026-02-10 11:51:22 -0500

    <feat>:<Ajout des classes prêtes à la migration>

---
+++ src/Domain/Entities/Module.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Module : AuditableAndSoftDeletableEntity
+    {
+        public string Nom { get; private set; } = null!;
+        public string Contenu { get; private set; } = null!;
+        public string Sujet { get; private set; } = null!;
+
+
+        public void SanitizeForSaving()
+        {
+            Nom = Nom.Trim();  
+            Contenu = Contenu.Trim();
+            Sujet = Sujet.Trim();
+        }
+
+    }
+}
