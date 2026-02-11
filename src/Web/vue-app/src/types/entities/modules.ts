7e397487fc2477f9f186a421d28253abcfe61c72
gog15 <george.gavrailov@gmail.com>
2026-02-11 14:55:21 -0500

    anjout du module en tant que class dans le front end et du store

---
+++ src/Web/vue-app/src/types/entities/modules.ts
@@ -0,0 +1,12 @@
+export interface IModules {
+    Nom : string
+  contenue : string
+  sujet : string 
+}
+
+export class Module implements IModules {
+  Nom : string=""
+  contenue: string=""
+  sujet : string=""  
+
+}
