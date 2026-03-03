77ee5b19d0f91c351e1680eb928be3ba5e01c14b
Ozias22 <oztas001@edu.cegepgarneau.ca>
2026-03-03 11:08:34 -0500

    Gestion de l'erreur 403

--- src/Web/vue-app/src/views/shared/Account.vue
+++ src/Web/vue-app/src/views/shared/Account.vue
@@ -1,13 +1,15 @@
 <template>
   <template v-if="userStore">
     <AdminAccount v-if="userStore.hasRole(Role.Admin)" />
-    <MemberAccount v-else-if="userStore.hasRole(Role.Member)" />
+    <!--<MemberAccount v-else-if="userStore.hasRole(Role.Member)" />-->
+    <MemberHome v-if="userStore.hasRole(Role.Member)"/>
   </template>
 </template>
 
 <script lang="ts" setup>
 import { Role } from "@/types/enums";
-import MemberAccount from "@/views/member/MemberAccount.vue";
+    //import MemberAccount from "@/views/member/MemberAccount.vue";
+import MemberHome from "@/views/member/MemberHome.vue"
 import AdminAccount from "@/views/admin/AdminAccount.vue";
 import { useUserStore } from "@/stores/userStore";
 
