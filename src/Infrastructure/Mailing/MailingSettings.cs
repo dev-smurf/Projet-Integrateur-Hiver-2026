e214e9cfc967af25a71151bbe59a30d4681f0fa0
George <92606286+gog15@users.noreply.github.com>
2026-04-20 13:12:48 -0400

    update :D

--- src/Infrastructure/Mailing/MailingSettings.cs
+++ src/Infrastructure/Mailing/MailingSettings.cs
@@ -2,7 +2,7 @@
 
 public class MailingSettings
 {
-    public Dictionary<string, string> FromName { get; set; } = null!;
-    public Dictionary<string, string> FromAddress { get; set; } = null!;
+    public string FromName { get; set; } = null!;
+    public string FromAddress { get; set; } = null!;
     public string ToAddressForDevelopment { get; set; } = null!;
 }
